f211d934a57c7cb922352ef1d1612c336a059b8c
Deepika Ankareddi <ankareddydeepika@gmail.com>
2020-05-16 17:30:22 -0400

    Added logice for Delete API

--- Develop/public/assets/js/notes.js
+++ Develop/public/assets/js/notes.js
@@ -10,16 +10,16 @@ fs.readFile("./db/db.json", "utf8", function(error, data) {
 });
 
 readdata()
-// const notesArray = {
-//     title: "Test hj",
-//     text: "Test textjj"
-// }
-// const jsonString = JSON.stringify(notesArray)
-// fs.writeFile("./db/db.json", jsonString, err => {
-//     if (err) {
-//         console.log('Error writing file', err)
-//     } else {
-//         console.log('Successfully wrote file')
-//     
+const notesArray = {
+    title: "Test hj",
+    text: "Test textjj"
+}
+const jsonString = JSON.stringify(notesArray)
+fs.writeFile("./db/db.json", jsonString, err => {
+    if (err) {
+        console.log('Error writing file', err)
+    } else {
+        console.log('Successfully wrote file')
+    
 
 module.exports = notesArray;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
-//var notesArray = require("./public/assets/js/notes");
+const { v4: uuidv4 } = require('uuid');
+//uuidv4();
 
 var fs = require("fs");
 var app = express();
@@ -11,66 +12,80 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-  fs.readFile("db/db.json", "utf8", function(error, data) {
+  fs.readFile("db/db.json", "utf8", function (error, data) {
 
-      if (error) {
-        return console.log(error);
-      }
-    
-      return res.json(JSON.parse(data))
-    
-    });
- 
+    if (error) {
+      return res.status(404).send();
+    }
+
+    return res.json(JSON.parse(data))
+
+  });
+
 });
 
-app.post("/api/notes", function(req, res) {
-  
+app.post("/api/notes", function (req, res) {
+
   var newnotes = req.body;
+  newnotes.id = uuidv4();
 
-    fs.readFile("db/db.json", "utf8", function(error, data) {
+  fs.readFile("db/db.json", "utf8", function (error, data) {
 
-      if (error) {
-        return console.log(error);
+    if (error) {
+      return res.status(404).send();
+    }
+
+    var previousdata = JSON.parse(data);
+
+    previousdata.push(newnotes);
+    const convert = JSON.stringify(newnotes);
+    const jsonString = JSON.stringify(previousdata)
+    fs.writeFile("db/db.json", jsonString, err => {
+      if (err) {
+        return res.status(404).send();
       }
-    
-      var previousdata = JSON.parse(data);
+      else {
+        return res.json(JSON.parse(convert))
 
-       previousdata.push(newnotes);
-       const convert = JSON.stringify(newnotes);
-       const jsonString = JSON.stringify(previousdata)
-       fs.writeFile("db/db.json",jsonString, err => {
-         if (err){
-           console.log('error writing file', err)
-         }
-         else{
-          return res.json(JSON.parse(convert))
-
-         }
-       })
-
-
-
-    
-    });
+      }
+    })
+  });
   
-
-
-
-//  const jsonString = JSON.stringify(newnotes)
-//  fs.appendFile("db/db.json", jsonString, err =>{
-//    if(err){
-//      console.log('Error writing File', err)
-//    }
-//    else{
-//      return res.json(JSON.parse(jsonString))
-//    }
-//  })
-
 });
+
+// DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
+  //This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, 
+  //you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+app.delete("/api/notes/:id", function(req, res){
+  
+  var deletenotes = req.params.id
+
+  fs.readFile("db/db.json", "utf8", function(error, data){
+    if(error){
+      return res.status(204).send();
+    }
+
+    var dbdata = JSON.parse(data);
+
+    var filterdata = dbdata.filter((note) => {return deletenotes !== note.id})
+    const jsonString = JSON.stringify(filterdata)
+    fs.writeFile("db/db.json", jsonString, err =>{
+      if(err){
+        return res.status(204).send();
+      }
+      else{
+        return res.status(200).send();
+      }
+    })
+    
+  })
+})
+
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -81,5 +96,5 @@ app.get("*", function (req, res) {
 
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
